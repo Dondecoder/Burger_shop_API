b72c5e07ee1bb25e6d629bfc51a1c2216570689c
Patrik Decoder <patrikfougere@gmail.com>
2022-02-15 01:20:56 -0500

    added SQL Alchemy

--- app.py
+++ app.py
@@ -2,22 +2,28 @@ from flask import Flask,app
 from flask_restful import  Api
 # from flask_jwt import JWT, jwt_required
 # from authentication import authenticate,identity
-from burgers import Burger,Allburgers
+from resources.burgers import Burger,Allburgers
+from db import db
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///burgers.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'Dondecoder'
 api = Api(app)
 
 
 # jwt = JWT(app,authenticate, identity)
 
-
-
+@app.before_first_request
+def create_tables():
+    db.create_all()
     
 api.add_resource(Burger, '/burger/<string:name>')
 api.add_resource(Allburgers, '/allburgers')
 
-app.run(port=5000, debug= True)
+if __name__ == '__main__':
+    db.init_app(app)
+    app.run(port=5000, debug= True)
 
 
